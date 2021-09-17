42f31fa0fa6fa530a88b1ae970665482352927d4
vladimirsk825 <stalinsk825@gmail.com>
2021-09-16 21:49:46 -0500

    Vladimir_Lamina_Angular_1_9_AliasVladimir_Lamina_Angular_1_9

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from 'src/app/services/api.service';
-import { UserService } from 'src/app/services/user.service';
+import { ApiService } from '@services/api.service';
+import { UserService } from '@services/user.service';
 
 @Component({
   selector: 'app-login',
